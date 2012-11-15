c87c54f19bd9449afc6f7da9e84887000fba8003
jb-alvarado <jb@pixelcrusher.de>
2012-11-15 20:37:54 +0100

    Add license in file

--- media_Compressor.bat
+++ media_Compressor.bat
@@ -1,9 +1,26 @@
-@echo off
-Setlocal EnableDelayedExpansion 
- 
-color 87
-title media_Compressor
- 
+::-------------------------------------------------------------------------------------
+:: LICENSE -------------------------------------------------------------------------
+::-------------------------------------------------------------------------------------
+::This Windows Batchscript is for the SendTo Menu. It compress audio and videos / folders with image sequences to mp4.
+::    Copyright (C) 2012  jb_alvarado
+::
+::    This program is free software: you can redistribute it and/or modify
+::    it under the terms of the GNU General Public License as published by
+::    the Free Software Foundation, either version 3 of the License, or
+::    (at your option) any later version.
+::
+::    This program is distributed in the hope that it will be useful,
+::    but WITHOUT ANY WARRANTY; without even the implied warranty of
+::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::    GNU General Public License for more details.
+::
+::    You should have received a copy of the GNU General Public License
+::    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::-------------------------------------------------------------------------------------
+::-------------------------------------------------------------------------------------
+
+::-------------------------------------------------------------------------------------
+:: History ---------------------------------------------------------------------------
 ::-------------------------------------------------------------------------------------
 ::
 :: This is version 0.95 from 2012-09-16. Last modification was on 2012-10-29
@@ -12,6 +29,13 @@ title media_Compressor
 ::
 ::-------------------------------------------------------------------------------------
 
+
+@echo off
+Setlocal EnableDelayedExpansion 
+ 
+color 87
+title media_Compressor
+ 
 :: EncoderSettings -------------------------------
 
 set preset=slow
